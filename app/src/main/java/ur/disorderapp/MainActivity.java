a6d70e13be8f4d6d91a759c2e1b080ffc1e354ae
Lauren <lpien@comcast.net>
2016-01-19 12:09:47 -0500

    Created drawer list and menu layout

--- app/src/main/java/ur/disorderapp/MainActivity.java
+++ app/src/main/java/ur/disorderapp/MainActivity.java
@@ -2,12 +2,27 @@ package ur.disorderapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawerList = (ListView)findViewById(R.id.left_drawer);
+        addDrawerItems();
+    }
+
+    //Helper method to create itemized list
+    private void addDrawerItems(){
+        String[] a = {"Module 1", "Module 2", "Module 3", "Module 4"};
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, a);
+        mDrawerList.setAdapter(mAdapter);
     }
 }
