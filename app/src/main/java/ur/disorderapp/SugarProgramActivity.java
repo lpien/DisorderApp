03e1c539c49a36cfe3ee650474ba3cceeb9172f7
Lauren <lpien@comcast.net>
2016-02-02 12:45:24 -0500

    added soda button

--- app/src/main/java/ur/disorderapp/SugarProgramActivity.java
+++ app/src/main/java/ur/disorderapp/SugarProgramActivity.java
@@ -12,10 +12,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class SugarProgramActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener
 {
+
+    Button mSodaTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +44,9 @@ public class SugarProgramActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //Soda button initialized
+        mSodaTask.setOnClickListener(new View.);
     }
 
     @Override
