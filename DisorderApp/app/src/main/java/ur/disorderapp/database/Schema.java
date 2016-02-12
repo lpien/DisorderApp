cff3e34ce5cddf3a8131dbfad6f03e8ca5a406c1
ShuyangLiu <sliu44@u.rochester.edu>
2016-02-12 00:41:45 -0500

    update schema

--- DisorderApp/app/src/main/java/ur/disorderapp/database/Schema.java
+++ DisorderApp/app/src/main/java/ur/disorderapp/database/Schema.java
@@ -13,7 +13,7 @@ public class Schema
          *  give it an empty constructor.
          */
     }
-
+    //A table keep track of the user's progress
     public static final class  GoalTable
     {
         public static final String NAME = "goals";
@@ -21,7 +21,35 @@ public class Schema
         public static final class Cols
         {
             public static final String STATUS = "status";
+
             public static final String PROGRESS = "progress";
         }
+    }
+    //A table record the user's living habits taken from their self-monitoring results
+    public static final class HabitTable
+    {
+        public static  final String NAME = "habit";
+
+        public static final class Cols
+        {
+            //Regarding to the question "Which of these did you eat?"
+            public static final String FOOD = "food";
+
+            //Regarding to the Question "How many?"
+            public static final String AMOUNT = "amount";
+
+            //Regarding to the Question "When?"
+            public static final String TIME = "time";
+
+            //Regarding to the Question "Where?"
+            public static final String LOCATION = "location";
+
+            //Regarding to the Question "What with?"
+            public static final String SITUATION = "situation";
+
+            //Regarding to the Question "What do you feel?"
+            public static final String FEELING = "feeling";
+        }
+
     }
 }
