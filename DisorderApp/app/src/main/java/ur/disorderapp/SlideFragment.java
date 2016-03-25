6c78bd81a620aaba140bcde1aaf960f9edec0894
Lauren <lpien@comcast.net>
2016-03-25 16:57:56 -0400

    mm

--- DisorderApp/app/src/main/java/ur/disorderapp/SlideFragment.java
+++ DisorderApp/app/src/main/java/ur/disorderapp/SlideFragment.java
@@ -9,7 +9,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.TextView;
 
 
@@ -54,9 +53,9 @@ public class SlideFragment extends Fragment
         int position = args.getInt(KEY);
 
         TextView titleText = (TextView) rootView.findViewById(R.id.title_text_view);
-        ImageButton btn_0 = (ImageButton) rootView.findViewById(R.id.frag_btn_0);
-        ImageButton btn_1 = (ImageButton) rootView.findViewById(R.id.frag_btn_1);
-        ImageButton btn_2 = (ImageButton) rootView.findViewById(R.id.frag_btn_2);
+        Button btn_0 = (Button) rootView.findViewById(R.id.frag_btn_0);
+        Button btn_1 = (Button) rootView.findViewById(R.id.frag_btn_1);
+        Button btn_2 = (Button) rootView.findViewById(R.id.frag_btn_2);
 
         //Button btn_3 = (Button) rootView.findViewById(R.id.frag_btn_3);
 
@@ -68,6 +67,7 @@ public class SlideFragment extends Fragment
 
         if(position == 0){
             titleText.setText("Which of these did you eat?");
+<<<<<<< HEAD
             btn_0.setImageResource(R.drawable.cocacola);
             btn_1.setImageResource(R.drawable.other);
             btn_2.setImageResource(R.drawable.other);
@@ -94,50 +94,62 @@ public class SlideFragment extends Fragment
             btn_1.setImageResource(R.mipmap.donut);
             btn_2.setImageResource(R.mipmap.coke);
             /*btn_0.setImageResource("A sugery drink");
+=======
+            btn_0.setText("A sugery drink");
+>>>>>>> parent of f5e3784... inserted images
             btn_1.setText("A pastry");
-            btn_2.setText("Other");*/
+            btn_2.setText("Other");
         }
         else if(position == 1){
             titleText.setText("How many did you consume?");
-            btn_0.setImageResource(R.mipmap.ic_launcher);
-            btn_1.setImageResource(R.mipmap.ic_launcher);
-            btn_2.setImageResource(R.mipmap.ic_launcher);
+            btn_0.setText("1");
+            btn_1.setText("2");
+            btn_2.setText("3");
         }
         else if(position == 2){
             titleText.setText("When did you eat it?");
-            btn_0.setImageResource(R.mipmap.morning);
-            btn_1.setImageResource(R.mipmap.afternoon);
-            btn_2.setImageResource(R.mipmap.night);
-            /*btn_0.setText("Morning");
+            btn_0.setText("Morning");
             btn_1.setText("Afternoon");
-            btn_2.setText("Evening");*/
+            btn_2.setText("Evening");
         }
         else if(position == 3){
             titleText.setText("Where did you eat it?");
+<<<<<<< HEAD
             btn_0.setImageResource(R.drawable.home);
             btn_1.setImageResource(R.drawable.work);
             btn_2.setImageResource(R.drawable.go);
            /* btn_0.setText("Home");
+=======
+            btn_0.setText("Home");
+>>>>>>> parent of f5e3784... inserted images
             btn_1.setText("Work");
-            btn_2.setText("On the go");*/
+            btn_2.setText("On the go");
         }
         else if(position == 4){
             titleText.setText("What did you eat it with?");
+<<<<<<< HEAD
             btn_0.setImageResource(R.drawable.meal);
             btn_1.setImageResource(R.drawable.other);
             btn_2.setImageResource(R.drawable.other);
             /*btn_0.setText("A meal");
+=======
+            btn_0.setText("A meal");
+>>>>>>> parent of f5e3784... inserted images
             btn_1.setText("Nothing");
-            btn_2.setText("Other");*/
+            btn_2.setText("Other");
         }
         else if(position == 5){
             titleText.setText("How were you feeling?");
+<<<<<<< HEAD
             btn_0.setImageResource(R.drawable.hungry);
             btn_1.setImageResource(R.drawable.thirsty);
             btn_2.setImageResource(R.drawable.notsure);
             /*btn_0.setText("Hungry");
+=======
+            btn_0.setText("Hungry");
+>>>>>>> parent of f5e3784... inserted images
             btn_1.setText("Thirsty");
-            btn_2.setText("Other");*/
+            btn_2.setText("Other");
         }
 
         return rootView;
